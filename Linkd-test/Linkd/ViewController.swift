04692e87969ba5d55729729f772b84b5058bfcaf
Rishabh Shah <shahrishabh7@utexas.edu>
2022-11-30 19:35:12 -0600

    Added image

--- Linkd-test/Linkd/ViewController.swift
+++ Linkd-test/Linkd/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let image = UIImage(named: "linkd-long-logo")
+        mainImage.image = image
     }
 
 
 }
-
